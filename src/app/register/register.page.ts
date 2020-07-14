3b9c14ec94566d459fab9b374b4cc1c8ff2ebfbd
fransolvam@gmail.com <fransolvam@gmail.com>
2020-07-14 13:23:23 +0200

    examen dia 14

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -15,11 +15,27 @@ export class RegisterPage implements OnInit {
   user: User ={uid:"", email:""}
 
   password:string;
+  valor:boolean=false;
 
   constructor(private dbuser:DbusersService, private authSvc: AuthService, private router: Router, public alertController: AlertController) { }
 
   ngOnInit() {
   }
+
+
+  
+ cambio(){
+  
+  if(this.valor==true){
+    this.valor=false;
+  }else if(this.valor==false){
+    this.valor=true;
+  }
+     console.log("cambiado");
+     console.log(this.valor);
+  
+}
+
 
   async mostraralert(fran:string){
     const alert = await this.alertController.create({
